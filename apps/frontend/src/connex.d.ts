32a8ab3cc274af52b41d03f5242ce8577915066c
Valazan <al-fede@hotmail.it>
2024-10-01 15:33:01 +0200

    feat: add options to signTypedData

--- apps/frontend/src/connex.d.ts
+++ apps/frontend/src/connex.d.ts
@@ -1,9 +1,14 @@
+interface SignTypedDataOptions {
+  signer?: string;
+}
+
 declare namespace Connex {
   interface Signer {
     signTypedData: (
       _domain: ethers.TypedDataDomain,
       _types: Record<string, ethers.TypedDataField[]>,
-      _value: Record<string, unknown>
+      _value: Record<string, unknown>,
+      _options?: SignTypedDataOptions
     ) => Promise<string>;
   }
 }
